693125cb2649c95ffaf8a2379f157c76d1ca66d8
aryakvipin <arya28985@gmail.com>
2023-09-22 19:41:19 +0530

    commit

--- lib/sapmle.dart
+++ lib/sapmle.dart
@@ -1,7 +1,13 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: sample(),));
+  runApp(DevicePreview(
+      builder: (BuildContext context) => MaterialApp(
+   useInheritedMediaQuery: true,
+            debugShowCheckedModeBanner: false,
+            home: sample(),
+          )));
 }
 
 class sample extends StatelessWidget {
@@ -9,19 +15,27 @@ class sample extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
 
-      appBar: AppBar(title: Text("My App"),),
-      body: Container(color: Colors.cyan,
-        child: Center(
-          child: Text("welcome to flutter", style: TextStyle(fontSize: 30,
-              color: Colors.red[900],
-              fontStyle: FontStyle.italic,
-              fontWeight:FontWeight.w600,),
+        body: Container(
+          color: Colors.cyan,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 300,left: 50,right: 50),
+            child: Center(
+              child: Column(
+                children: [
+                   Image(image: AssetImage("images/Simpleicons.png"),height: 100,width: 100,),
+                  Text(
+                    "welcome to flutter",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.red[900],
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
-      )
-
-    );
+        ));
   }
-
-
 }
